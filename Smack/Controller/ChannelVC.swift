28986d3ae3752a0cd225ca4ebece6717872b5591
CanHaskan <canhaskan1903@hotmail.com>
2025-10-08 00:24:12 +0300

    Baslangic

---
+++ Smack/Controller/ChannelVC.swift
@@ -0,0 +1,22 @@
+//
+//  ChannelVC.swift
+//  Smack
+//
+//  Created by Can Haskan on 16.03.2025.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 80
+
+    }
+    
+
+   
+
+}
---
+++ Smack/Controller/ChatVC.swift
@@ -0,0 +1,25 @@
+//
+//  ChatVC.swift
+//  Smack
+//
+//  Created by Can Haskan on 16.03.2025.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+    
+    
+    @IBOutlet weak var menuBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+
+    }
+    
+
+}
