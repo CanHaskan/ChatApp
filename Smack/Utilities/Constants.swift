13a536cabac2de35cd43b623ac9e7f80cfff148e
CanHaskan <canhaskan1903@hotmail.com>
2025-10-13 22:27:23 +0300

    v4

--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -7,7 +7,19 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+//URL Constants
+let URL_REGISTER = "http://localhost:3005/v1/account/register"
+//let URL_REGISTER = "ht/tp://192.168.1.22:localhost:3005/v1/account/register"
+
 //Segues
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+
+// User Defaults
+
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIN"
+let USER_EMAIL = "userEmail"
